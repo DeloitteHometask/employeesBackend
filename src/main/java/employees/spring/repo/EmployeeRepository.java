b73124f723cab92456063b6ad3ee2826059b30ec
Elena-Kravtsova <kravtsovaeyu@gmail.com>
2023-11-14 19:34:23 +0200

    Updated Query (added search by composite pattern)

--- src/main/java/employees/spring/repo/EmployeeRepository.java
+++ src/main/java/employees/spring/repo/EmployeeRepository.java
@@ -12,17 +12,31 @@ import employees.spring.entity.EmployeeEntity;
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 	
 	List<EmployeeEntity> findAll(Sort sort);
-	
-	@Query(value = "SELECT * FROM employees e "
-			+ "LEFT JOIN worktitles wt ON e.worktitles_id = wt.work_title "
-			+ "WHERE (LOWER(e.name) LIKE CONCAT(LOWER(:pattern), '%') "
-			+ "OR LOWER(e.name) LIKE CONCAT('% ', LOWER(:pattern), '%') "
-			+ "OR LOWER(wt.work_title) LIKE CONCAT(LOWER(:pattern), '%') "
-			+ "OR LOWER(wt.work_title) LIKE CONCAT('% ', LOWER(:pattern), '%')) "
-			+ "ORDER BY e.name ASC "
-			+ "LIMIT :limit "
-			+ "OFFSET :offset", nativeQuery = true)
-	List<EmployeeEntity> findEmployeesByPattern(@Param("pattern") String pattern, int limit, int offset);
+
+	@Query(value = 
+		    "SELECT * FROM employees e "
+		    + "LEFT JOIN worktitles wt ON e.worktitles_id = wt.work_title "
+		    + "WHERE ("
+		    + "(POSITION(' ' IN LOWER(:pattern)) = 0 AND "
+		    + "(LOWER(e.name) LIKE CONCAT(LOWER(:pattern), '%') "
+		    + "OR LOWER(e.name) LIKE CONCAT('% ', LOWER(:pattern), '%') "
+		    + "OR LOWER(wt.work_title) LIKE CONCAT(LOWER(:pattern), '%') "
+		    + "OR LOWER(wt.work_title) LIKE CONCAT('% ', LOWER(:pattern), '%'))) "
+		    + "OR (POSITION(' ' IN LOWER(:pattern)) > 0 "
+		    + "AND ((LOWER(e.name) LIKE CONCAT(SUBSTRING(LOWER(:pattern), 1, POSITION(' ' IN LOWER(:pattern)) - 1), '%') "
+		    + "OR LOWER(e.name) LIKE CONCAT('% ', SUBSTRING(LOWER(:pattern), 1, POSITION(' ' IN LOWER(:pattern)) - 1), '%')) "
+		    + "AND (LOWER(e.name) LIKE CONCAT('% ', TRIM(LEADING ' ' FROM SUBSTRING(LOWER(:pattern) FROM POSITION(' ' IN LOWER(:pattern)) + 1)), '%') "
+		    + "OR LOWER(wt.work_title) LIKE CONCAT('% ', TRIM(LEADING ' ' FROM SUBSTRING(LOWER(:pattern) FROM POSITION(' ' IN LOWER(:pattern)) + 1)), '%')) "
+		    + "OR (LOWER(wt.work_title) LIKE CONCAT(SUBSTRING(LOWER(:pattern), 1, POSITION(' ' IN LOWER(:pattern)) - 1), '%') "
+		    + "OR LOWER(wt.work_title) LIKE CONCAT('% ', SUBSTRING(LOWER(:pattern), 1, POSITION(' ' IN LOWER(:pattern)) - 1), '%')) "
+		    + "AND (LOWER(wt.work_title) LIKE CONCAT('% ', TRIM(LEADING ' ' FROM SUBSTRING(LOWER(:pattern) FROM POSITION(' ' IN LOWER(:pattern)) + 1)), '%') "
+		    + "OR LOWER(e.name) LIKE CONCAT('% ', TRIM(LEADING ' ' FROM SUBSTRING(LOWER(:pattern) FROM POSITION(' ' IN LOWER(:pattern)) + 1)), '%'))))) "
+		    + "ORDER BY e.name ASC "
+		    + "LIMIT :limit OFFSET :offset", 
+		    nativeQuery = true)
+		List<EmployeeEntity> findEmployeesByPattern(@Param("pattern") String pattern, int limit, int offset);
+
+
 
 
 }
